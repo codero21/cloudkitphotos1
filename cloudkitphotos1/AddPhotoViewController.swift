8fefdd7878281a01d7f1fb3ad7f1ab81da3cd323
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-08-03 00:19:00 -0400

    Build user interface for application.

---
+++ cloudkitphotos1/AddPhotoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddPhotoViewController.swift
+//  cloudkitphotos1
+//
+//  Created by Rollin Francois on 8/2/17.
+//  Copyright © 2017 Francois Technology. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: Action Methods
+    
+    @IBAction func onSelectPicture(_ sender: Any) {
+    }
+  
+    @IBAction func onSaveRecord(_ sender: Any) {
+    }
+    
+    @IBAction func onCancel(_ sender: Any) {
+    }
+    
+}
--- cloudkitphotos1/DetailViewController.swift
+++ cloudkitphotos1/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  cloudkitphotos1
 //
 //  Created by Rollin Francois on 8/2/17.
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
-
+class DetailViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UIButton!
+    @IBOutlet weak var detailLabel: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
